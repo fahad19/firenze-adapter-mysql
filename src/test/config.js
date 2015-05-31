d05bace1f5ec12cc6570c4c40bc6a64c252bb9cc
Fahad Ibnay Heylaal <fahad19@gmail.com>
2015-05-31 19:49:47 +0200

    config make configure-able via CLI.

--- src/test/config.js
+++ src/test/config.js
@@ -2,10 +2,10 @@ var Mysql = require('../index');
 
 module.exports = {
   adapter: Mysql,
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'firenze',
+  host: process.env.FIRENZE_HOST || '127.0.0.1',
+  user: process.env.FIRENZE_USER || 'root',
+  password: process.env.FIRENZE_PASS || '',
+  database: process.env.FIRENZE_DB || 'firenze',
   pool: {
     min: 0,
     max: 1
