4748c262fae95cf896a53cd62c9d8984cdb25822
Fahad Ibnay Heylaal <fahad19@gmail.com>
2015-08-22 03:16:25 +0200

    upgrade to firenze v0.2.1

---
+++ src/test/models/Author.js
@@ -0,0 +1,4 @@
+/* eslint-disable new-cap */
+var f = require('firenze');
+
+module.exports = f.createModelClass({});
---
+++ src/test/models/Post.js
@@ -0,0 +1,5 @@
+/*eslint camelcase: [2, {properties: "never"}]*/
+/* eslint-disable new-cap */
+var f = require('firenze');
+
+module.exports = f.createModelClass({});
